b7b0e4837a3ad6b233b91414a7bd8c44b4c6074c
Matthew Branton <mbranton@emberfinancial.com>
2015-12-02 17:20:29 -0500

    added memory backed multi-ets storage option

---
+++ src/bitjar_storage_ets.erl
@@ -0,0 +1,119 @@
+%% (c) 2015, Matthew Branton
+%% BSD 2-clause, check /LICENSE
+%% Generic storage interface
+
+%% Memory Based Storage
+
+-module(bitjar_storage_ets).
+
+-include_lib("bitjar.hrl").
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
+-behaviour(bitjar).
+
+-export([bitjar_init/1,
+		 bitjar_shutdown/1,
+		 bitjar_store/2,
+		 bitjar_lookup/2,
+		 bitjar_delete/2,
+		 bitjar_all/2,
+		 bitjar_filter/5,
+		 bitjar_foldl/4,
+		 bitjar_default_options/0]).
+
+-define(GROUP_ID, 1).
+
+bitjar_init(Options) ->
+	{ok, #{refmap => #{},
+		   options => lists:merge(bitjar_default_options(), Options)}, ?GROUP_ID, #{}}. 
+
+bitjar_shutdown(#bitjar{state = #{refmap := R}}) ->
+	maps:fold(fun(_K, V, _AccIn) -> true = ets:delete(V) end, ok, R),
+	ok.
+
+bitjar_default_options() -> [set, {keypos, 1}, {read_concurrency, true}].
+
+bitjar_store(#bitjar{}=B, StoreList) -> store(B, StoreList).
+
+bitjar_lookup(#bitjar{}=B, LookupList) -> lookup(B, LookupList, length(LookupList), []).
+
+bitjar_delete(#bitjar{}=B, DeleteList) -> delete(B, DeleteList).
+
+bitjar_all(#bitjar{}=B, GroupId) -> all(B, GroupId).
+
+bitjar_filter(B, FilterFuns, GroupId, KdeserialFun, VdeserialFun) -> filter(B, FilterFuns, GroupId, KdeserialFun, VdeserialFun).
+
+bitjar_foldl(#bitjar{}=B, Fun, Start, GroupId) -> foldl(B, Fun, Start, GroupId).
+
+
+
+%% Internal Functions
+
+store(B, []) -> {ok, B};
+store(#bitjar{state=#{refmap := R}=S}=B, [{Id, K, V}|T]) ->
+	case do_store(maps:find(Id, R), B, K, V) of
+		ok -> store(B, T);
+		{newref, Ref} -> store(B#bitjar{state=S#{refmap => maps:put(Id, Ref, R)}}, T)
+	end.
+
+do_store(error, #bitjar{state=#{options := O}}, K, V) ->
+	lager:info("OPTIONS: ~p~n", [O]),
+	Ref = ets:new(jar, O),
+	true = ets:insert(Ref, {K, V}),
+	{newref, Ref};
+
+do_store({ok, Ref}, _B, K, V) ->
+	true = ets:insert(Ref, {K, V}),
+	ok.
+
+lookup(_B, [], _L, []) -> not_found;
+lookup(_B, [], L, Acc) when L =:= length(Acc) -> {ok, Acc};
+lookup(_B, [], _L, Acc) -> {partial, Acc};
+
+lookup(#bitjar{state=#{refmap := RefMap}}=B, [{GroupName, GroupId, Key}|T], L, Acc) ->
+	AccOut = lookup_ets(maps:find(GroupId, RefMap), GroupName, Key, Acc),
+	lookup(B, T, L, AccOut).
+
+lookup_ets(error, _GroupName, _Key, Acc) -> Acc;
+lookup_ets({ok, Ref}, GroupName, Key, Acc) ->
+	case ets:lookup(Ref, Key) of
+		[{K,V}] -> [{GroupName, K, V}|Acc];
+		_ -> Acc
+	end.
+
+delete(B, []) -> {ok, B};
+delete(#bitjar{state=#{refmap := R}}=B, [{_GroupName, Id, K}|T]) ->
+	ets_delete(maps:find(Id, R), K),
+	delete(B, T).
+
+ets_delete(error, _Key) -> ok;
+ets_delete({ok, Ref}, Key) ->
+	true = ets:delete(Ref, Key).
+
+all(#bitjar{state=#{refmap := R}}, GroupId) ->
+	case maps:find(GroupId, R) of
+		error -> [];
+		{ok, Ref} -> ets:tab2list(Ref)
+	end.
+
+
+filter(B, FilterFuns, GroupId, KdeserialFun, VdeserialFun) ->
+	foldl(B, fun(K, V, Acc) ->
+					 K2 = KdeserialFun(K),
+					 V2 = VdeserialFun(V),
+					 case bitjar_helper:run_fundefs(FilterFuns, {K2, V2}) of
+					 	 true -> [V|Acc];
+					 	 false -> Acc
+					 end
+			 end, [], GroupId).
+
+foldl(#bitjar{state=#{refmap := R}}, Fun, Start, GroupId) ->
+	ets_foldl(maps:find(GroupId, R), Fun, Start).
+
+ets_foldl(error, _, Start) -> Start;
+ets_foldl({ok, Ref}, Fun, Start) ->
+	lists:reverse(ets:foldl(fun({K,V}, Acc) -> Fun(K, V, Acc) end, Start, Ref)).
+
+
---
+++ test/bitjar_ets_tests.erl
@@ -0,0 +1,124 @@
+%% (c) 2015, Matthew Branton
+%% BSD 2-clause, check /LICENSE
+
+
+-module(bitjar_ets_tests).
+-author('mbranton@emberfinancial.com').
+
+-define(PATH, "/tmp/bitjartests").
+
+-include_lib("../include/bitjar.hrl").
+-include_lib("eunit/include/eunit.hrl").
+
+start() -> ok.
+stop(_) -> ok.
+
+init() ->
+	{ok, B} = bitjar:open(ets, []),
+	?assertMatch(#bitjar{}, B),
+	?assertEqual(ok, bitjar:close(B)).
+
+store() ->
+  {ok, B} = bitjar:open(ets, []),
+  {ok, B2} = bitjar:store(B, test, <<"key">>, <<"value">>),
+  {ok, B3} = bitjar:store(B2, test, <<"key2">>, <<"value2">>),
+  {ok, B4} = bitjar:store(B3, test2, <<"key">>, <<"value2">>),
+  {ok, B5} = bitjar:store(B4, test3, <<"key">>, <<"value2">>),
+  {ok, B6} = bitjar:store(B5, test3, <<"key2">>, <<"value2">>),
+  G = bitjar:groups(B6),
+  ?assertEqual(3, maps:size(G)),
+  ok = bitjar:close(B),
+  {ok, C} = bitjar:open(ets, []),
+  G2 = bitjar:groups(C),
+  ?assertEqual(0, maps:size(G2)),
+  ok = bitjar:close(C).
+
+lookup() ->
+	{ok, B} = bitjar:open(ets, []),
+	{ok, B2} = bitjar:store(B, test, <<"key">>, <<"value">>),
+	{ok, B3} = bitjar:store(B2, test, <<"key2">>, <<"value">>),
+	{ok, B4} = bitjar:store(B3, test2, <<"key">>, <<"value">>),
+	{ok, [{test2, <<"key">>, <<"value">>}]} = bitjar:lookup(B4, test2, <<"key">>),
+	{partial, _} = bitjar:lookup(B4, [{test, <<"key">>}, {test, <<"key2">>}, {test, <<"key3">>}]),
+	{ok, _} = bitjar:lookup(B4, [{test, <<"key">>}, {test, <<"key2">>}, {test2, <<"key">>}]),
+	ok = bitjar:close(B4).
+
+delete() ->
+	{ok, B} = bitjar:open(ets, []),
+	{ok, B2} = bitjar:store(B, test, <<"key">>, <<"value">>),
+	{ok, [{test, <<"key">>, <<"value">>}]} = bitjar:lookup(B2, test, <<"key">>),
+	{ok, B3} = bitjar:delete(B2, test, <<"key">>),
+	?assertMatch(not_found, bitjar:lookup(B3, test, <<"key">>)),
+	ok = bitjar:close(B3).
+
+all() ->
+	{ok, B} = bitjar:open(ets, []),
+	?assertMatch([], bitjar:all(B, missinggroup)),
+	{ok, B2} = bitjar:store(B, test, <<"key">>, <<"value">>),
+	{ok, B3} = bitjar:delete(B2, test, <<"key">>),
+	?assertMatch([], bitjar:all(B3, testgroup)),
+	?assertMatch({ok, _}, maps:find(test, bitjar:groups(B3))),
+	{ok, B4} = bitjar:store(B3, test, <<"key2">>, <<"value2">>),
+	{ok, B5} = bitjar:store(B4, test2, <<"k">>, <<"v">>),
+	{ok, B6} = bitjar:store(B5, test2, <<"k2">>, <<"v2">>),
+	?assertMatch([{<<"key2">>, <<"value2">>}], bitjar:all(B6, test)),
+	?assertMatch([{<<"k">>, <<"v">>}, {<<"k2">>, <<"v2">>}], bitjar:all(B6, test2)),
+	ok = bitjar:close(B6).
+	
+foldl() ->
+	{ok, B} = bitjar:open(ets, []),
+	?assertMatch([], bitjar:foldl(B, fun(_K, _V, _Acc) -> ok end, [], test)),
+	{ok, B2} = bitjar:store(B, test, <<"mykey">>, <<"myvalue">>),
+	?assertMatch([{folded, <<"mykey">>, <<"myvalue">>}],
+				 bitjar:foldl(B2, fun(K,V,Acc) -> [{folded, K, V}|Acc] end, [], test)),
+	ok = bitjar:close(B2).
+
+filter() ->
+	{ok, B} = bitjar:open(ets, []),
+	{ok, B2} = bitjar:store(B, test, <<"mykey">>, <<"myvalue">>),
+	{ok, B3} = bitjar:store(B2, test, <<"mykey2">>, <<"myvalue2">>),
+	{ok, B4} = bitjar:store(B3, test, <<"mykey3">>, <<"myvalue3">>),
+	?assertMatch([], bitjar:filter(B4, fun({_, _}) -> false end, test)),
+	?assertMatch([_,_,_], bitjar:filter(B4, fun({_,_}) -> true end, test)),
+	ok = bitjar:close(B4).
+
+multistore() ->
+	{ok, B} = bitjar:open(ets, []),
+	{ok, B2} = bitjar:store(B, [{test, <<"mykey">>, <<"myvalue">>},
+								{test, <<"m2">>, <<"k2">>},
+								{test, <<"m3">>, <<"k3">>}]),
+	?assertMatch([_,_,_], bitjar:all(B2, test)),
+	ok = bitjar:close(B2).
+
+serialization() ->
+	{ok, B} = bitjar:open(ets, []),
+	SerialFun = fun(E) -> erlang:term_to_binary(E) end,
+	DeserialFun = fun(E) -> erlang:binary_to_term(E) end,
+	{ok, B2} = bitjar:set_serializer(B, test,
+						  SerialFun, %% Key Serializer
+						  SerialFun, %% Value Serializer
+						  DeserialFun, %% Key Deserializer
+						  DeserialFun), %% Value Deserializer
+	{ok, B3} = bitjar:store(B2, [{test, mykey, "myvalue"}]),
+	?assertEqual({ok, [{test, mykey, "myvalue"}]},
+				 bitjar:lookup(B3, test, mykey)),
+	ok = bitjar:close(B2).
+
+leveldb_test_() -> 
+  {foreach,
+  fun start/0,
+  fun stop/1,
+   [
+            {"Bitjar init", fun init/0},
+			{"Store values", fun store/0},
+			{"lookup", fun lookup/0},
+			{"delete", fun delete/0},
+			{"all", fun all/0},
+			{"fold", fun foldl/0},
+			{"filter", fun filter/0},
+			{"multistore", fun multistore/0},
+			{"serialization", fun serialization/0}
+   ]}.
+
+%%%%% Memory / ETS bitjar backend
+%%%
